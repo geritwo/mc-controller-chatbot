31b4140508fdfb9e1ee0f5535f0f214e0a2dab58
Gergo Vamosi <geritwo@gmail.com>
2022-11-19 19:31:15 +0100

    Use // to invoke

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='//', intents=intents)
 
 
 @bot.command(name="foo", description="Sends back whatever you type in")

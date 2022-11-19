539a9ae5a896b00fc449579bb1f2013b2626d65a
Gergo Vamosi <geritwo@gmail.com>
2022-11-19 19:27:16 +0100

    add foo

--- bot.py
+++ bot.py
@@ -1,18 +1,18 @@
-# bot.py
 import os
-import random
 
 import discord
 from discord.ext import commands
 
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 
-@bot.command()
-async def foo(ctx, arg):
-    await ctx.send(arg)
+@bot.command(name="foo", description="Sends back whatever you type in")
+async def foo(ctx, arg=""):
+    res = arg if arg else "bar"
+    await ctx.send(res)
 
 bot.run(TOKEN)

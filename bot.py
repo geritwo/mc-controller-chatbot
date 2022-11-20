4dad3d68c18be8a7aeecaed4b0433daf9e34c7b3
Gergo Vamosi <geritwo@gmail.com>
2022-11-20 10:12:57 +0100

    Update intents so that bot replies in channe,

--- bot.py
+++ bot.py
@@ -7,12 +7,25 @@ from discord.ext import commands
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='//', intents=intents)
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='//', intents=intents, description="A simple Discord bot")
 
 
 @bot.command(name="foo", description="Sends back whatever you type in")
 async def foo(ctx, arg=""):
     res = arg if arg else "bar"
     await ctx.send(res)
+
+
+@bot.command(name="hello", description="Sends a hello to user")
+async def hello_user(ctx):
+    await ctx.send(f"Hello {ctx.author}!")
+
+
+@bot.command(name="server", description="Check server stats")
+async def server_control(ctx, arg):
+    pass
 
 bot.run(TOKEN)
